1d747a020a956999abc654031846319c7c2138b6
Lars Lorenz <lars.ll.lorenz@gmail.com>
2019-05-05 14:59:44 +0200

    Fix MicroDaysClockBase calculation

--- microdaysclockbase.py
+++ microdaysclockbase.py
@@ -3,12 +3,12 @@ from clock import Clock
 
 class MicroDaysClockBase(Clock):
     def __init__(self, ms_in_d: int) -> None:
-        self._ms_in_md = ms_in_d // 1000
-        self._ms_in_ud = self._ms_in_md // 1000
+        self._ms_in_md = ms_in_d / 1000
+        self._ms_in_ud = self._ms_in_md / 1000
 
     def name(self) -> str:
         return f"Microdays-based Clock with {self._ms_in_ud} ms in a ud"
 
     def format_time(self, ms_since_midnight: int) -> str:
-        return f"{ms_since_midnight // self._ms_in_md:03d}:" \
-                f"{(ms_since_midnight % self._ms_in_md) // self._ms_in_ud:03d}"
+        return f"{ms_since_midnight // self._ms_in_md:03.0f}:" \
+            f"{(ms_since_midnight % self._ms_in_md) // self._ms_in_ud:03.0f}"
