4db6ff4e01eed187ad2a493985fc3ae3c591c39c
Lars Lorenz <lars.ll.lorenz@gmail.com>
2019-05-05 13:49:29 +0200

    Add MetaWakingHoursClock

--- wakinghoursclock.py
+++ wakinghoursclock.py
@@ -1,26 +1,12 @@
+from abc import ABCMeta
+from metawakinghoursclock import MetaWakingHoursClock
 from twentyfourhourclock import TwentyFourHourClock
 
-MS_IN_S = 1000
-MS_IN_MIN = 60 * MS_IN_S
-MS_IN_H = 60 * MS_IN_MIN
-MS_IN_D = 24 * MS_IN_H
-DAY_BEGIN_IN_MS = 7 * 60 * 60 * 1000
-DAY_END_IN_MS = 23 * 60 * 60 * 1000
+
+class ABCMetaWakingHoursClock(ABCMeta, MetaWakingHoursClock):
+    pass
 
 
-class WakingHoursClock(TwentyFourHourClock):
+class WakingHoursClock(TwentyFourHourClock, metaclass=ABCMetaWakingHoursClock):
     def name(self) -> str:
         return "Waking Hours Clock"
-
-    def format_time(self, ms_since_midnight: int) -> str:
-        if ms_since_midnight >= DAY_BEGIN_IN_MS \
-                and ms_since_midnight < DAY_END_IN_MS:
-            ms_since_wakeup = ms_since_midnight - DAY_BEGIN_IN_MS
-            return super().format_time(ms_since_wakeup)
-        else:
-            if ms_since_midnight < DAY_BEGIN_IN_MS:
-                ms_since_falling_asleep = ms_since_midnight + MS_IN_D \
-                        - DAY_END_IN_MS
-            else:
-                ms_since_falling_asleep = ms_since_midnight - DAY_END_IN_MS
-            return f"-{super().format_time(ms_since_falling_asleep)}"
