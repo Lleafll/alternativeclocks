86e336a86a6dc7105f36bda2e261c9b57689f229
Lars Lorenz <lars.ll.lorenz@gmail.com>
2019-05-05 14:47:46 +0200

    Add MicroDaysClockBase

--- microdaysclock.py
+++ microdaysclock.py
@@ -1,15 +1,11 @@
-from clock import Clock
+from microdaysclockbase import MicroDaysClockBase
 
-MD_IN_D = 1000  # MD = millidays
 MS_IN_D = 24 * 60 * 60 * 1000
-MS_IN_MD = MS_IN_D // MD_IN_D
-MS_IN_UD = MS_IN_MD // 1000
 
 
-class MicroDaysClock(Clock):
+class MicroDaysClock(MicroDaysClockBase):
+    def __init__(self) -> None:
+        super().__init__(MS_IN_D)
+
     def name(self) -> str:
         return "Microdays-based Clock"
-
-    def format_time(self, ms_since_midnight: int) -> str:
-        return f"{ms_since_midnight // MS_IN_MD:03d}:" \
-                f"{(ms_since_midnight % MS_IN_MD) // MS_IN_UD:03d}"
